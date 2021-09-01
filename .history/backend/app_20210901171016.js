ab4ce5726ddd4c4cc046c69ea1b2e27d8d71d2f8
Massine <massinoueslati@gmail.com156>
2021-09-01 19:46:20 +0200

    connexion avec MongoDB Atlas + installation mongoose + correction sur dotenv et tests sur les serveurs  + ajout du midleware CORS

---
+++ .history/backend/app_20210901171016.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+
+//les variables d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+const MY_PORT = process.env.PORT;
+const MY_APP_SECRET = process.env.APP_SECRET;
+
+
+
+//Connexion à mongoDB Atlas via mangoose
+mongoose.connect('mongodb+srv://massine15:5YZB751uLJDkn2Tw@oc-p6.oi0ad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Middleware pour CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.get("/", (req, res) => {
+  return res.send(MY_APP_SECRET);
+});
+
+app.listen(MY_PORT, () => console.log(`Server running on port ${MY_PORT}`));
+
+app.use((req, res) => {
+    res.json({
+        message: 'Votre requête a bien été reçue !'
+    });
+});
+
+module.exports = app;
---
+++ .history/backend/app_20210901174647.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+
+//les variables d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+const MY_PORT = process.env.PORT;
+const MY_APP_SECRET = process.env.APP_SECRET;
+
+
+
+//Connexion à mongoDB Atlas via mangoose
+mongoose.connect('mongodb+srv://massine15:5YZB751uLJDkn2Tw@oc-p6.oi0ad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  
+//Middleware pour CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({
+        message: 'Votre requête a bien été reçue !'
+    });
+    next();
+});
+
+app.get("/", (req, res, next) => {
+  return res.send(MY_APP_SECRET);
+  next();
+});
+app.listen(MY_PORT, () => console.log(`Serveuuuuurr running on port ${MY_PORT}`));
+
+module.exports = app;
